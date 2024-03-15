9942c4e9f0da6c4f71e0c0bd71f304a6f3467ba3
Daniele Mottura <dany.mottu@gmail.com>
2024-03-15 17:29:10 +0100

    Improve

--- Client.py
+++ Client.py
@@ -2,6 +2,7 @@ import requests, socket
 import tkinter as tk
 
 
+
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 class App():   
@@ -11,18 +12,31 @@ class App():
         self.root.geometry("300x200")
         self.root.title("Chat")
         self.message = tk.Entry(self.root)
-        btn = tk.Button(self.root,text="Send Ip",command=self.sendIp)
+        btn = tk.Button(self.root,text="Send text",command=self.sendMessage)
         self.message.grid(row=0, column=0)
         btn.grid(row=1,column=0)
+        btn2 = tk.Button(self.root, text="show chat", command=self.see_chat)
+        btn2.grid(row=2, column=0)
         self.root.mainloop()
         pass
 
-    def sendIp(self):
+    def sendMessage(self):
         url = "http://192.168.1.193:5000"
         text = self.message.get()
         value = {"Ip": self.ip, "Text": text}
         x = requests.post(url, data=value)
-        
-        
+    
+    def see_chat(self):
+        url = "http://192.168.1.193:5000/chat"
+        x = requests.get(url)
+        text = tk.Label(self.root, text=x.text)
+        text.grid(row=3, column=0)
 
 App(ip)
+
+"""
+TO-DO list
+- automate the text show
+- make it preattier
+- add a censur measur
+"""
--- Server.py
+++ Server.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
+
+all_ip = []
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         Ip = request.form["Ip"]
+        all_ip.append(Ip)
         Message = request.form["Text"]
         file = open("History.txt", "r")
         backup = file.read()
@@ -14,6 +18,13 @@ def home():
         file.write(f"{backup}\n{Ip}: {Message}")
         file.close()
     return "hello"
+
+@app.route("/chat")
+def chat():
+    file = open("History.txt", "r")
+    current_chat = file.read()
+    file.close()
+    return f"current_chat"
 
 
 if __name__ == "__main__":
