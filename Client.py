fb9aa165e1f7bd97b3de4eece19b6d046b5e9ccf
Daniele Mottura <dany.mottu@gmail.com>
2024-03-30 20:43:45 +0100

    A little better graphic

--- Client.py
+++ Client.py
@@ -1,22 +1,23 @@
 import requests
 import tkinter as tk
+from tkinter import ttk 
 
 class App():   
     def  __init__(self) -> None:
         self.root = tk.Tk()
         self.root.geometry("500x300")
         self.root.title("Chat")
-        self.message = tk.Entry(self.root)
-        btn = tk.Button(self.root,text="Send text",command=self.sendMessage)
-        user_text = tk.Label(self.root, text="User:")
-        message_text = tk.Label(self.root, text="Message:")
-        self.user = tk.Entry(self.root)
+        self.message = ttk.Entry(self.root)
+        btn = ttk.Button(self.root,text="Send text",command=self.sendMessage)
+        user_text = ttk.Label(self.root, text="User:")
+        message_text = ttk.Label(self.root, text="Message:")
+        self.user = ttk.Entry(self.root)
         self.user.grid(row=0, column=1)
         user_text.grid(row=0, column=0)
         message_text.grid(row=0,column=2)
         self.message.grid(row=0, column=3)
         btn.grid(row=1,column=0)
-        btn2 = tk.Button(self.root, text="show chat", command=self.see_chat)
+        btn2 = ttk.Button(self.root, text="show chat", command=self.see_chat)
         btn2.grid(row=2, column=0)
         self.root.mainloop()
         pass
@@ -37,7 +38,7 @@ class App():
         message = message.replace(")", "")
         message = message.replace("[", "")
         message = message.replace("]", "")        
-        text = tk.Label(self.root, text=message)
+        text = ttk.Label(self.root, text=message)
         text.grid(row=3, column=0)
 
 App()
