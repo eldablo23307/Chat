1239ee2a2494a43c81d64966017c32bb59cf087f
Daniele Mottura <dany.mottu@gmail.com>
2024-04-02 15:05:06 +0200

    Add censur

--- Server.py
+++ Server.py
@@ -4,6 +4,67 @@ from Databasehandle import Write_In_DataBase, Return_From_DataBase
 
 app = Flask(__name__)
 all_user = []
+swear_words = [
+    "arse",
+    "arsehead",
+    "arsehole",
+    "ass",
+    "asshole",
+    "bastard",
+    "bitch",
+    "bloody",
+    "bollocks",
+    "brotherfucker",
+    "bugger",
+    "bullshit",
+    "child-fucker",
+    "cock",
+    "cocksucker",
+    "crap",
+    "cunt",
+    "cyka blyat",
+    "damn",
+    "damn it",
+    "dick",
+    "dickhead",
+    "dyke",
+    "fatherfucker",
+    "frigger",
+    "fuck",
+    "goddamn",
+    "godsdamn",
+    "hell",
+    "holy shit",
+    "horseshit",
+    "in shit",
+    "Jesus Christ",
+    "Jesus fuck",
+    "Jesus H. Christ",
+    "Jesus Harold Christ",
+    "Jesus, Mary and Joseph",
+    "Jesus wept",
+    "kike",
+    "motherfucker",
+    "nigga",
+    "nigra",
+    "pigfucker",
+    "piss",
+    "prick",
+    "pussy",
+    "shit",
+    "shit ass",
+    "shite",
+    "sisterfucker",
+    "slut",
+    "son of a whore",
+    "son of a bitch",
+    "spastic",
+    "sweet Jesus",
+    "turd",
+    "twat",
+    "wanker"
+]
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -11,6 +72,10 @@ def home():
         user = request.form["user"]
         all_user.append(user)
         Message = request.form["Text"]
+        hold = Message.split()
+        for i in hold:
+            if i in swear_words:
+                Message = Message.replace(i, "******")
         Write_In_DataBase(user, Message)
 
     return "hello"
