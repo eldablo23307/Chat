051e958ecca4e2ebe009cf24b671d2b12557a5cc
Daniele Mottura <dany.mottu@gmail.com>
2024-04-04 16:02:35 +0200

    Better Graphics

--- Client.py
+++ Client.py
@@ -1,23 +1,23 @@
 import requests
-import tkinter as tk
-from tkinter import ttk 
+import customtkinter as ctk
 
 class App():   
     def  __init__(self) -> None:
-        self.root = tk.Tk()
+        self.root = ctk.CTk()
+        ctk.set_appearance_mode("dark")
         self.root.geometry("500x300")
         self.root.title("Chat")
-        self.message = ttk.Entry(self.root)
-        btn = ttk.Button(self.root,text="Send text",command=self.sendMessage)
-        user_text = ttk.Label(self.root, text="User:")
-        message_text = ttk.Label(self.root, text="Message:")
-        self.user = ttk.Entry(self.root)
+        self.message = ctk.CTkEntry(self.root)
+        btn = ctk.CTkButton(self.root,text="Send text",command=self.sendMessage)
+        user_text = ctk.CTkLabel(self.root, text="User:")
+        message_text = ctk.CTkLabel(self.root, text="Message:")
+        self.user = ctk.CTkEntry(self.root)
         self.user.grid(row=0, column=1)
         user_text.grid(row=0, column=0)
         message_text.grid(row=0,column=2)
         self.message.grid(row=0, column=3)
         btn.grid(row=1,column=0)
-        btn2 = ttk.Button(self.root, text="show chat", command=self.see_chat)
+        btn2 = ctk.CTkButton(self.root, text="show chat", command=self.see_chat)
         btn2.grid(row=2, column=0)
         self.root.mainloop()
         pass
@@ -38,7 +38,7 @@ class App():
         message = message.replace(")", "")
         message = message.replace("[", "")
         message = message.replace("]", "")        
-        text = ttk.Label(self.root, text=message)
+        text = ctk.CTkLabel(self.root, text=message)
         text.grid(row=3, column=0)
 
 App()
@@ -46,5 +46,4 @@ App()
 """
 TO-DO list
 - automate the text show
-- add a censur measur
 """
