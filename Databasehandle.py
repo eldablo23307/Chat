8523ebad819cc6e53a3e6d0d86e7191ac588acd8
eldablo23307 <dany.mottu@gmail.com>
2025-07-10 14:04:24 +0200

    Sistemato il database

--- Databasehandle.py
+++ Databasehandle.py
@@ -1,37 +1,11 @@
-import mysql.connector
+from tinydb import TinyDB, Query
 
-def Write_In_DataBase(user, Message):
-    mydb = mysql.connector.connect(host="Software99.mysql.pythonanywhere-services.com", user="Software99", password="M0ttu2307", database="Software99$default")
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE TABLE IF NOT EXISTS messaggi (user VARCHAR(255), messagio VARCHAR(255));")
-    sql = "INSERT INTO messaggi (user, messagio) VALUES (%s, %s);"
-    val = (user, Message)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    mycursor.close()
-    mydb.close()
+def Write_In_DataBase(user, message):
+    db = TinyDB('database.json')
+    db.insert({"user": user, "message": message})
 
 def Return_From_DataBase():
-    mydb = mysql.connector.connect(host="Software99.mysql.pythonanywhere-services.com", user="Software99", password="M0ttu2307", database="Software99$default")
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM messaggi;")
-    messages = mycursor.fetchall() # Ottenere tutti i messaggi
-    mycursor.close()
-    mydb.close()
+    db = TinyDB('datatbase.json')
+    messages = db.all()
     return messages
 
-def Write_email(email):
-    mydb = mysql.connector.connect(
-        host="Software99.mysql.pythonanywhere-services.com",
-        user="Software99",
-        password="M0ttu2307",
-        database="Software99$User_info"
-    )
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE TABLE IF NOT EXISTS mail (email VARCHAR(255));")
-    sql = "INSERT INTO mail (email) VALUES (%s);"
-    val = (email,)  # Assicurati che val sia una tupla con una virgola finale
-    mycursor.execute(sql, val)
-    mydb.commit()
-    mycursor.close()
-    mydb.close()
